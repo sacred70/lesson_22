2a34610dea64c084546d3233fa83256516d96bd0
sacred70 <sacred70@mail.ru>
2023-03-19 10:44:14 +0700

    fly/crawl

--- practice/bad_smell_comments/main.py
+++ practice/bad_smell_comments/main.py
@@ -7,54 +7,50 @@
 
 class Unit:
     # ...
-    def mvmntobjbfld(self, field, feld_1_param, field_2_param, d, fl, cr, points_per_action = 1):
-        """Функция реализует перемещение юнита по полю. в качестве параметров принимает текущие координаты юнита, 
-        направление его движения, состояние не летит ли он, состояние не крадется ли он и базовый параметр скорости с 
-        которым двигается юнит
-        :param field: поле по которому перемещается юнит
-        :param feld_1_param: x-координата юнита
-        :param field_2_param: у- координата юнита
-        :param d: направление перемещения
-        :param fl: летит ли юнит
-        :param cr: крадется ли юнит
-        :param points_per_action: базовый параметр скорости
+    def moving_user_on_field(self, field,
+                             x_coordin,
+                             y_coordin,
+                             direction,
+                             fly_user,
+                             crawling_user,
+                             speed_default=1):
         """
-        # Для начала проверим не установлены ли у нас два флага полета и подкрадывания в истину,
-        # т.к. одновременно эти события не должны происходить
-        if fl and cr:  # если оба параметра установлены как True
-            # выбросим ошибку
+        Функция реализует перемещение юнита по полю.
+        """
+        # Проверка состояния пользователя, летит/ползет
+        if fly_user and crawling_user:
             raise ValueError('Рожденный ползать летать не должен!')
-        # Если ошибку мы не выбросили, значит все у нас ок и в принципе мы можем переходить к дальнейшему выполнению кода
 
-        if fl:  # для начала реализуем логику для состояния когда мы летим
-            points_per_action *= 1.2 # раз мы летим то сцепления наших тапочек с землей нет, и следовательно трение меньше, и поэтому скорость выше
-            if d == 'UP':  # если направление нашего движения установлено как UP
-                new_y = field_2_param + points_per_action   # увеличим нашу координату Y на нашу текущую скорость
-                new_x = feld_1_param  # оставим нашу координату Х без изменений
-            elif d == 'DOWN':  # если направление нашего движения установлено как DOWN
-                new_y = field_2_param - points_per_action   # уменьшим нашу координату Y на нашу текущую скорость
-                new_x = feld_1_param  # оставим нашу координату Х без изменений
-            elif d == 'LEFT':  # если направление нашего движения установлено как LEFT
-                new_y = field_2_param  # оставим нашу координату Y без изменений
-                new_x = feld_1_param - points_per_action # уменьшим нашу координату Х на нашу текущую скорость
-            elif d == 'RIGHT':  # если направление нашего движения установлено как RIGHT
-                new_y = field_2_param  # оставим нашу координату Y без изменений
-                new_x = feld_1_param + points_per_action # увеличим нашу координату Х на нашу текущую скорость
-        if cr:
-            points_per_action *= 0.5
-            if d == 'UP':  # если направление нашего движения установлено как UP
-                new_y = field_2_param + points_per_action  # увеличим нашу координату Y на нашу текущую скорость
-                new_x = feld_1_param  # оставим нашу координату Х без изменений
-            elif d == 'DOWN':  # если направление нашего движения установлено как DOWN
-                new_y = field_2_param - points_per_action  # уменьшим нашу координату Y на нашу текущую скорость
-                new_x = feld_1_param  # оставим нашу координату Х без изменений
-            elif d == 'LEFT':  # если направление нашего движения установлено как LEFT
-                new_y = field_2_param  # оставим нашу координату Y без изменений
-                new_x = feld_1_param - points_per_action  # уменьшим нашу координату Х на нашу текущую скорость
-            elif d == 'RIGHT':  # если направление нашего движения установлено как RIGHT
-                new_y = field_2_param  # оставим нашу координату Y без изменений
-                new_x = feld_1_param + points_per_action  # увеличим нашу координату Х на нашу текущую скорость
+        # логика в режиме полета
+        if fly_user:
+            speed_default *= 1.2  # в полете скорость выше
+            if direction == 'UP':  # движение вверх
+                new_y = y_coordin + speed_default  # увеличим нашу координату Y на нашу текущую скорость
+                new_x = x_coordin  # Х без изменений
+            elif direction == 'DOWN':  # движение вниз
+                new_y = y_coordin - speed_default  # уменьшим нашу координату Y на нашу текущую скорость
+                new_x = x_coordin  # Х без изменений
+            elif direction == 'LEFT':  # движение влево
+                new_y = y_coordin  # Y без изменений
+                new_x = x_coordin - speed_default  # уменьшим нашу координату Х на нашу текущую скорость
+            elif direction == 'RIGHT':  # движение вправо
+                new_y = y_coordin  # Y без изменений
+                new_x = x_coordin + speed_default  # увеличим нашу координату Х на нашу текущую скорость
+        if crawling_user:
+            speed_default *= 0.5  # ползком скорость ниже
+            if direction == 'UP':  # движение вверх
+                new_y = y_coordin + speed_default  # увеличим нашу координату Y на нашу текущую скорость
+                new_x = x_coordin  # Х без изменений
+            elif direction == 'DOWN':  # движение вниз
+                new_y = y_coordin - speed_default  # уменьшим нашу координату Y на нашу текущую скорость
+                new_x = x_coordin  # Х без изменений
+            elif direction == 'LEFT':  # движение влево
+                new_y = y_coordin  # Y без изменений
+                new_x = x_coordin - speed_default  # уменьшим нашу координату Х на нашу текущую скорость
+            elif direction == 'RIGHT':  # движение вправо
+                new_y = y_coordin  # Y без изменений
+                new_x = x_coordin + speed_default  # увеличим нашу координату Х на нашу текущую скорость
 
-            field.set_unit(x=new_x, y=new_y, unit=self)
+            field.set_unit(x=new_x, y=new_y, unit=self)  # новые координаты положения пользователя
 
 #     ...
